b696d1864567e550ad0d3dbc1f87e470d9bd0e82
kdncrts <kdncrts@gmail.com>
2020-02-07 18:59:10 -0700

    fix

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const accountRoutes = require('./routes/accounts');
-const serviceRoutes = require('./routes/services');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use('/account', accountRoutes);
-app.use('/services', serviceRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
     res.render(
